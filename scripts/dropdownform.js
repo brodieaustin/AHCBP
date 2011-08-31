6828261a2273bffafffb97d14c26199022634ea3
Brodie Austin <brodie.austin@gmail.com>
2011-08-31 01:05:08 -0500

    working on download page

--- scripts/dropdownform.js
+++
@@ -1,5 +0,0 @@
-function surfto(form) {
-var myindex=form.dropdownoption.selectedIndex
-if (form.dropdownoption.options[myindex].value != 0) {
-location=form.dropdownoption.options[myindex].value;}
-}
