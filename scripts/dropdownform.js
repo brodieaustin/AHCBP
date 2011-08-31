686e7dd8af9924f76b9725dbbab118ec7f182d5d
Brodie Austin <brodie.austin@gmail.com>
2011-08-31 01:07:03 -0500

    adding back scripts

---
+++ scripts/dropdownform.js
@@ -0,0 +1,5 @@
+function surfto(form) {
+var myindex=form.dropdownoption.selectedIndex
+if (form.dropdownoption.options[myindex].value != 0) {
+location=form.dropdownoption.options[myindex].value;}
+}
